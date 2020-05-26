0977ff2989b91dd6e36b5bfe5485e976a616df4a
aranchai <35885720+aranchai@users.noreply.github.com>
2020-05-26 22:50:16 +0700

    Updates

---
+++ precache-manifest.9ff351c940d1f25f8add1c0dfa2cea7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627cb62cab069e68caa681ee672636bb",
+    "url": "/AingCodeForEducation/index.html"
+  },
+  {
+    "revision": "8eada463147c905b59f1",
+    "url": "/AingCodeForEducation/static/css/main.c69474eb.chunk.css"
+  },
+  {
+    "revision": "c4709864df69edbeb4cc",
+    "url": "/AingCodeForEducation/static/js/2.f6257565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AingCodeForEducation/static/js/2.f6257565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eada463147c905b59f1",
+    "url": "/AingCodeForEducation/static/js/main.b71de5d8.chunk.js"
+  },
+  {
+    "revision": "399091b91b82a57cb5db",
+    "url": "/AingCodeForEducation/static/js/runtime-main.f2e23308.js"
+  }
+]);
